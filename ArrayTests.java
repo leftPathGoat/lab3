7d1d49b1a7a94061b84348d5239391780868862b
Francis Li <sol026@ucsd.edu>
2023-04-20 19:05:51 -0700

    lab3 submodule

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,18 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReverseInPlace2()
+  {
+    int[] input1 = { 3, 6, 9 , 12 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ 12,9, 6, 3 }, input1);
+  }
+
+  @Test
+  public void testReversed2()
+  {
+
+  }
 }
